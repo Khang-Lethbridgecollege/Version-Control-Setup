7a0d71bd9b24f0eab079f0d5e41f76199e994fd9
Khang-Lethbridgecollege <72067596+Khang-Lethbridgecollege@users.noreply.github.com>
2020-12-05 21:34:33 -0700

    optimal performance
    Increase game performance through fixing frame, physics, changing static and dynamic settings

---
+++ Assets/PerformanceBounceback/Scripts/BallSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    
+
+    public GameObject pooledBall; //the prefab of the object in the object pool
+    public static BallSpawner SharedInstance;
+    public List<GameObject> pooledObjects;
+    public static int ballNumber = 0;
+    public int amountToPool;
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tmp = Instantiate(pooledBall);
+            tmp.SetActive(false);
+            pooledObjects.Add(tmp);
+          
+        }
+        InvokeRepeating("SpawnBall", 0f, 0.5f);
+    }
+    
+   	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+
+
+    public GameObject GetPooledObject()
+    {
+        //1
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            //2
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        //3   
+        return null;
+    }
+
+
+    void SpawnBall()
+    {
+        GameObject ball = BallSpawner.SharedInstance.GetPooledObject();
+        if (ball != null)
+        {
+            ball.transform.position = transform.position;
+            ball.transform.rotation = transform.rotation;
+            ball.SetActive(true);
+        }
+       /*
+        ballNumber++;
+        if (ballNumber >= amountToPool)
+        {
+            amountToPool = 0;
+        }
+       */
+    }
+
+
+}
